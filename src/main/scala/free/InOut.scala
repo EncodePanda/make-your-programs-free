b09ff4140645ba360f69064d7257c5d21eef872d
Pawel Szulc <paul.szulc@gmail.com>
2016-09-11 16:27:42 +0100

    Lift InOut to Free

--- src/main/scala/free/InOut.scala
+++ src/main/scala/free/InOut.scala
@@ -6,3 +6,9 @@ sealed trait InOut[A]
 case class PrintLine(line: String) extends InOut[Unit]
 case object GetLine extends InOut[String]
 
+
+object InOut {
+  def printLine(line: String): Free[InOut, Unit] = Free.liftF(PrintLine(line))
+  def getLine(): Free[InOut, String] = Free.liftF(GetLine)
+}
+
