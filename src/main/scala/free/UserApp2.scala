bcefe6bd5d384ccfa5346d8310c64dc4b9760516
Pawel Szulc <paul.szulc@gmail.com>
2016-09-13 09:55:47 +0100

    UserApp2 first interpreter

--- src/main/scala/free/UserApp2.scala
+++ src/main/scala/free/UserApp2.scala
@@ -19,4 +19,18 @@ object UserApp2 extends App {
 
   val prog = program[Eff]
 
+  def interpreter[S[_]](implicit
+    s0: KVS[Long, UserAccount, ?] :<: S,
+    s1: MonotonicSeq :<: S,
+    s2: Task :<: S
+  ): Eff ~> Free[S, ?] = {
+    type G[A] = Free[S, A]
+
+    val first: InOut ~> G = InOutInterpreter.interpreter[S]
+    val second: ManipulateAccount ~> G =  MAInterpreters.interpreter[S]
+
+    first :+: second
+  }
+
+
 }
